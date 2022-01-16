75e5145b3cd249426e7b030a5b7d85735d560cc7
Rafael Freitas <gothsonn@hotmail.com>
2022-01-16 18:36:08 -0300

    'update'

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CoreRoutingModule } from './core-routing.module';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    FooterComponent,
+    HeaderComponent
+  ],
   imports: [
     CommonModule,
     CoreRoutingModule
